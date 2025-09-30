ed485a107424eadf42243006e8212a2bd0a449c9
dmdcl <diegogael13@hotmail.com>
2025-09-29 19:27:39 -0600

    refactor: add collectors orchestation of ec2 resources

--- aws_inventory/regional/ec2.py
+++ aws_inventory/regional/ec2.py
@@ -1,65 +1,39 @@
+# EC2 inventory collector - orchestrates all EC2 resource collection
 from aws_inventory.utils.boto_helpers import create_session
-from aws_inventory.utils.common import get_name
-
+from aws_inventory.collectors.security_groups import collect_security_groups
+from aws_inventory.collectors.instances import collect_instances
+from aws_inventory.collectors.vpc import (
+    collect_internet_gateways,
+    collect_subnets,
+    collect_vpcs
+)
 
 def collect_ec2(profile, region):
     """
-    Collect EC2 inventory for a given region.
-    Returns structured data (dict/list) instead of HTML.
+    Collect EC2 inventory for a given region
+
+    This function orchrestates the collection of all EC2 resources
+    - Security Groups
+    - Instances
+    - VPCs
+    - Subnets
+    - Internet Gateways
+
+    Args:
+        profile AWS profile name
+        region: AWS region name
+
+    Returns: 
+        list: List of VPC dictionaries with all nested resources
     """
     session = create_session(profile)
-    ec2 = session.client("ec2", region_name=region)
+    ec2 = session.client("ec2, region_name=region")
 
-    vpcs = ec2.describe_vpcs()["Vpcs"]
-    subnets = ec2.describe_subnets()["Subnets"]
-    instances = ec2.describe_instances()["Reservations"]
-    igws = ec2.describe_internet_gateways()["InternetGateways"]
-
-    inventory = []
-
-    for vpc in vpcs:
-        vpc_id = vpc["VpcId"]
-        vpc_entry = {
-            "id": vpc_id,
-            "name": get_name(vpc.get("Tags")),
-            "cidr": vpc.get("CidrBlock"),
-            "subnets": [],
-            "igws": [
-                {
-                    "id": igw["InternetGatewayId"], 
-                    "name": get_name(igw.get("Tags"))
-                }
-                for igw in igws
-                if any(att["VpcId"] == vpc_id for att in igw["Attachments"])
-            ],
-        }
-
-        for subnet in [s for s in subnets if s["VpcId"] == vpc_id]:
-            subnet_entry = {
-                "id": subnet["SubnetId"],
-                "name": get_name(subnet.get("Tags")),
-                "cidr": subnet.get("CidrBlock"),
-                "az": subnet.get("AvailabilityZone"),
-                "instances": [],
-            }
-
-            for r in instances:
-                for i in r["Instances"]:
-                    if i["SubnetId"] == subnet["SubnetId"]:
-                        subnet_entry["instances"].append(
-                            {
-                                "id": i["InstanceId"],
-                                "name": get_name(i.get("Tags")),
-                                "type": i["InstanceType"],
-                                "state": i["State"]["Name"],
-                                "private_ip": i.get("PrivateIpAddress"),
-                                "public_ip": i.get("PublicIpAddress"),
-                                "sgs": [sg["GroupId"] for sg in i["SecurityGroups"]],
-                            }
-                        )
-
-            vpc_entry["subnets"].append(subnet_entry)
-
-        inventory.append(vpc_entry)
+    # Collect resources in dependency order
+    sg_map = collect_security_groups(ec2)
+    instances_by_subnet = collect_instances(ec2, sg_map)
+    igws_by_vpc = collect_internet_gateways(ec2)
+    subnets_by_vpc = collect_subnets(ec2, instances_by_subnet)
+    inventory = collect_vpcs(ec2, subnets_by_vpc, igws_by_vpc, sg_map)
 
     return inventory
